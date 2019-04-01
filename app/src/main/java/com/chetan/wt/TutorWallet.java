5be59d8d7337ee4a244cc48f7b8e5ca204698fc3
Ankit Jain <anku.171co208@nitk.edu.in>
2019-04-01 07:19:00 +0530

    wallet

--- app/src/main/java/com/chetan/wt/TutorWallet.java
+++ app/src/main/java/com/chetan/wt/TutorWallet.java
@@ -1,13 +1,17 @@
 package com.chetan.wt;
 
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,9 +23,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class TutorWallet extends AppCompatActivity {
 
-    DatabaseReference reff;
+    DatabaseReference reff, reff_trans;
+    Transaction trans;
     String TutorID;
     Button addMoney;
     EditText money;
@@ -53,29 +60,71 @@ public class TutorWallet extends AppCompatActivity {
             }
         });
 
-        addMoney=findViewById(R.id.addMoney);
-        money=findViewById(R.id.money);
+        addMoney = findViewById(R.id.addMoney);
+        money = findViewById(R.id.money);
 
         addMoney.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(money.getVisibility()==View.VISIBLE)
-                {
-                    temp = Integer.parseInt(money.getText().toString());
-                    if(walletBalance + temp>100000)
-                        Toast.makeText(getApplicationContext(),"Maximum Wallet Limit is 100000",Toast.LENGTH_SHORT).show();
-                    else{
-                        reff.child("wallet").setValue(walletBalance + temp);
+                if (money.getVisibility() == View.VISIBLE) {
+                    if (money.getText().toString().length() == 0)
                         money.setVisibility(View.GONE);
-                        Toast.makeText(getApplicationContext(), "Money Successfully Added", Toast.LENGTH_SHORT).show();
+                    else {
+                        temp = Integer.parseInt(money.getText().toString());
+                        if (walletBalance + temp > 100000)
+                            Toast.makeText(getApplicationContext(), "Maximum Wallet Limit is 100000", Toast.LENGTH_SHORT).show();
+                        else {
+                            reff.child("wallet").setValue(walletBalance + temp);
+                            money.setVisibility(View.GONE);
+                            Toast.makeText(getApplicationContext(), "Money Successfully Added", Toast.LENGTH_SHORT).show();
+                        }
                     }
-                }
-                else
+                } else
                     money.setVisibility(View.VISIBLE);
             }
         });
 
+
+        final ListView listView = (ListView) findViewById(R.id.Transactions);
+        final ArrayList<String> myArrayList = new ArrayList<>();
+        reff_trans = FirebaseDatabase.getInstance().getReference("Tutor Transactions").child(TutorID);
+
+        final ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myArrayList)
+        {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text = (TextView) view.findViewById(android.R.id.text1);
+                text.setTextColor(Color.WHITE);
+                return view;
+            }
+        };
+        reff_trans.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                myArrayList.clear();
+                for(DataSnapshot ds: dataSnapshot.getChildren())
+                {
+                    trans = ds.getValue(Transaction.class);
+                    myArrayList.add("\n" + trans.getAmount()+ trans.getName()+ trans.getCourse_name()+"\n"+trans.getDate()+"\n");
+                }
+                listView.setAdapter(myArrayAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
     }
+
+
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
